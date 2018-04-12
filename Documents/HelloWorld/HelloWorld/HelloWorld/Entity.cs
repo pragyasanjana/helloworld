78a6d4006a7e196e93f09bb485a008f17efcac9e
Anupam Agarwal <anupam.agarwal@nagarro.com>
2018-04-12 15:28:16 +0530

    test cases

---
+++ Documents/HelloWorld/HelloWorld/HelloWorld/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld
+{
+    public class Entity
+    {
+        public string ConcatenateName(string firstName = "", string lastName ="")
+        {
+            return firstName + " " + lastName;
+        }
+    }
+}
---
+++ Documents/HelloWorld/HelloWorld/UnitTests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            int a = 2;
+            int b = 3;
+
+            Assert.AreEqual(5, a + b);
+        }
+
+        [TestMethod]
+        public void TestConcatenateNames()
+        {
+            Entity entity = new Entity();
+
+            Assert.AreEqual("Hello World", entity.ConcatenateName("Hello", "World"));
+
+        }
+    }
+}
